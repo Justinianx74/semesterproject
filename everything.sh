c1ee243d48e68683d12677158b72d76b941a1598
Justin Davis <Justin.Davis@slu.edu>
2019-03-29 17:13:33 -0500

    Initial commit for everything.sh. This is the script that is gonna run all the other scripts. I'm gonna put this in my startup

---
+++ everything.sh
@@ -0,0 +1,7 @@
+printf 'Running weather script\n\n'
+bash ~/Documents/school/cs2930/semesterproject/weather.sh
+printf '\n'
+sleep 2
+printf 'Running word script\n\n'
+bash ~/Documents/school/cs2930/semesterproject/word.sh
+sleep 2
