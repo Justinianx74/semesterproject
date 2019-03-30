ecd19a3e5ea76def382b333886aa3357cc3b2b2a
Justin Davis <Justin.Davis@slu.edu>
2019-03-30 11:59:38 -0500

    Add - to before the author

--- quote.sh
+++ quote.sh
@@ -4,6 +4,6 @@ quote=$(cat index.html | egrep '<dailyquote>' -A 2 | head -3 | egrep '<p>' | sed
 printf 'The quote of the day is:''\n'
 echo $quote
 author=$(cat index.html | egrep '<p class="author">' -A 1 | sed 's/<[^>]*>//g' | sed 's/\&#8211; //g' | head -2 | tail -n 1 | sed 's/\&nbsp;//g' | sed 's/^ //')
-echo $author
+echo '-'$author
 
 rm index.html
