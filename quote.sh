a29518e53d585d551ca6cd0b9b994d3321a98b9e
Justin Davis <Justin.Davis@slu.edu>
2019-03-30 14:05:09 -0500

    Adds quotes to the output

--- quote.sh
+++ quote.sh
@@ -2,7 +2,7 @@ wget -q https://www.eduro.com/
 
 quote=$(cat index.html | egrep '<dailyquote>' -A 2 | head -3 | egrep '<p>' | sed 's/<[^>]*>//g')
 printf 'The quote of the day is:''\n'
-echo $quote
+echo '"'$quote'"'
 author=$(cat index.html | egrep '<p class="author">' -A 1 | sed 's/<[^>]*>//g' | sed 's/\&#8211; //g' | head -2 | tail -n 1 | sed 's/\&nbsp;//g' | sed 's/^ //')
 echo '-'$author
 
