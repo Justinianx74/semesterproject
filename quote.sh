666da64c6eb5c528cd0c41e86be177c1ffdeba3b
Justin Davis <Justin.Davis@slu.edu>
2019-03-30 11:57:41 -0500

    Add quote script. Finds quote of the day from a webstie and gets the quote, and gets the author

---
+++ quote.sh
@@ -0,0 +1,9 @@
+wget -q https://www.eduro.com/
+
+quote=$(cat index.html | egrep '<dailyquote>' -A 2 | head -3 | egrep '<p>' | sed 's/<[^>]*>//g')
+printf 'The quote of the day is:''\n'
+echo $quote
+author=$(cat index.html | egrep '<p class="author">' -A 1 | sed 's/<[^>]*>//g' | sed 's/\&#8211; //g' | head -2 | tail -n 1 | sed 's/\&nbsp;//g' | sed 's/^ //')
+echo $author
+
+rm index.html
