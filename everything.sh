786228fc0971d8b689ead53c9148b41935c6d043
Justin Davis <Justin.Davis@slu.edu>
2019-03-30 11:58:00 -0500

    update everything.sh to include quote.sh

--- everything.sh
+++ everything.sh
@@ -5,3 +5,8 @@ sleep 2
 printf 'Running word script\n\n'
 bash ~/Documents/school/cs2930/semesterproject/word.sh
 sleep 2
+printf '\nRunning quote script\n\n'
+bash ~/Documents/school/cs2930/semesterproject/quote.sh
+sleep 2
+printf '\n'
+
