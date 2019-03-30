413310d3fda9c3927d439794fab8e31bdda99f6c
Justin Davis <Justin.Davis@slu.edu>
2019-03-30 14:20:40 -0500

    Adds highs and lows for tomorrow

--- weather.sh
+++ weather.sh
@@ -7,4 +7,7 @@ low=$(cat wx.php | egrep 'p class="temp low">Low: [0-9]{1,3}&deg;F</p>' | egrep 
 echo 'The low today is' $low
 summary=$(cat wx.php | egrep '<p class="summary">[^<]*</p>' | sed 's/<[^>]*>\([^<]*\)<\/p>/\1/')
 echo "It's currently" ${summary,,} "right now"
+tomhigh=$(cat wx.php | egrep 'p class="temp high">High: [0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}' | head -2 | tail -1)
+tomlow=$(cat wx.php | egrep 'p class="temp low">Low: [0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}' | head -2 | tail -1)
+echo 'Tomorrow there is a high of' $tomhigh', and  a low of' $tomlow
 rm wx.php
