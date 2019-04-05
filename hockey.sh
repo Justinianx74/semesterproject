520512d9131e17347ee13177e23d938e4018e43a
Justin Davis <Justin.Davis@slu.edu>
2019-04-05 14:42:34 -0500

    Changes the wget command to be silent

--- hockey.sh
+++ hockey.sh
@@ -1,5 +1,5 @@
 #start
 wget -q www.cbssports.com/nhl/scoreboard/
-newline=$(cat index.html | egrep '<a href="/nhl/scoreboard/' | tail -3 | head -1 | egrep -o '/nhl/scoreboard/[^/]*/' | sed 's/.*/wget www.cbssports.com&/')
+newline=$(cat index.html | egrep '<a href="/nhl/scoreboard/' | tail -3 | head -1 | egrep -o '/nhl/scoreboard/[^/]*/' | sed 's/.*/wget -q www.cbssports.com&/')
 rm index.html
 echo $newline | bash -s
