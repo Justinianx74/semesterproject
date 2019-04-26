9994c963c0b07933c26eafeba6d2e742056c36c2
Justin Davis <Justin.Davis@slu.edu>
2019-04-26 13:56:52 -0500

    Change to using backticks instead of dollar sign and paranthesis. Change variables to all uppercase

--- weather.sh
+++ weather.sh
@@ -1,13 +1,13 @@
 wget -q http://www.rssweather.com/zipcode/63103/wx.php
-currentTemp=$(cat wx.php | egrep '<p class="temp">[0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}')
-echo 'The current temperature is' $currentTemp
-high=$(cat wx.php | egrep 'p class="temp high">High: [0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}' | head -1)
-echo 'The high today is' $high
-low=$(cat wx.php | egrep 'p class="temp low">Low: [0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}' | head -1)
-echo 'The low today is' $low
-summary=$(cat wx.php | egrep '<p class="summary">[^<]*</p>' | sed 's/<[^>]*>\([^<]*\)<\/p>/\1/')
-echo "It's currently" ${summary,,} "right now"
-tomhigh=$(cat wx.php | egrep 'p class="temp high">High: [0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}' | head -2 | tail -1)
-tomlow=$(cat wx.php | egrep 'p class="temp low">Low: [0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}' | head -2 | tail -1)
-echo 'Tomorrow there is a high of' $tomhigh', and  a low of' $tomlow
+CURRENTTEMP=`cat wx.php | egrep '<p class="temp">[0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}'`
+echo 'The current temperature is' $CURRENTTEMP
+HIGH=`cat wx.php | egrep 'p class="temp high">High: [0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}' | head -1`
+echo 'The high today is' $HIGH
+LOW=`cat wx.php | egrep 'p class="temp low">Low: [0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}' | head -1`
+echo 'The low today is' $LOW
+SUMMARY=`cat wx.php | egrep '<p class="summary">[^<]*</p>' | sed 's/<[^>]*>\([^<]*\)<\/p>/\1/'`
+echo "It's currently" ${SUMMARY,,} "right now"
+TOMHIGH=`cat wx.php | egrep 'p class="temp high">High: [0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}' | head -2 | tail -1`
+TOMLOW=`cat wx.php | egrep 'p class="temp low">Low: [0-9]{1,3}&deg;F</p>' | egrep -o '[0-9]{1,3}' | head -2 | tail -1`
+echo 'Tomorrow there is a high of' $TOMHIGH', and  a low of' $TOMLOW
 rm wx.php
--- word.sh
+++ word.sh
@@ -1,8 +1,8 @@
 wget -q https://www.merriam-webster.com/word-of-the-day
-word=$(cat word-of-the-day | egrep '<div class="word-and-pronunciation">' -A 1 | egrep -o '<h1>[^<]*</h1>' | sed 's/<[^>]*>//g')
-echo 'The word of the day is' $word
-main=$(cat word-of-the-day | egrep '<span class="main-attr">' | sed 's/<[^>]*>//g')
-echo 'It is a' $main
+WORD=`cat word-of-the-day | egrep '<div class="word-and-pronunciation">' -A 1 | egrep -o '<h1>[^<]*</h1>' | sed 's/<[^>]*>//g'`
+echo 'The word of the day is' $WORD
+MAIN=`cat word-of-the-day | egrep '<span class="main-attr">' | sed 's/<[^>]*>//g'`
+echo 'It is a' $MAIN
 printf 'It means "'
 cat word-of-the-day | egrep '<h2>Definition</h2>' -A 1 | sed 's/<[^>]*>//g' | sed "s/Definition//" | sed 's/^[^:]*://' | sed 's/^\s*/ /' | tr -d '\n' | sed 's/: //g' | sed 's/^ *//' | sed 's/^.*$/&"/'
 echo
