d74be619e74f55962a1664692e8ae8bef81252f5
Justin Davis <Justin.Davis@slu.edu>
2019-04-28 14:02:19 -0500

    Change to backticks and allow for multiline quotes

--- quote.sh
+++ quote.sh
@@ -1,9 +1,9 @@
 wget -q https://www.eduro.com/
 
-quote=$(cat index.html | egrep '<dailyquote>' -A 2 | head -3 | egrep '<p>' | sed 's/<[^>]*>//g')
+QUOTE=`cat index.html | egrep '<dailyquote>' -A 3 | head -4 | sed 's/<[^>]*>//g' | sed 's/^\s*//' | sed 's/\s*$//'`
 printf 'The quote of the day is:''\n'
-echo '"'$quote'"'
-author=$(cat index.html | egrep '<p class="author">' -A 1 | sed 's/<[^>]*>//g' | sed 's/\&#8211; //g' | head -2 | tail -n 1 | sed 's/\&nbsp;//g' | sed 's/^ //')
-echo '-'$author
+echo '"'$QUOTE'"'
+AUTHOR=`cat index.html | egrep '<p class="author">' -A 1 | sed 's/<[^>]*>//g' | sed 's/\&#8211; //g' | head -2 | tail -n 1 | sed 's/\&nbsp;//g' | sed 's/^ //'`
+echo '-'$AUTHOR
 
 rm index.html
