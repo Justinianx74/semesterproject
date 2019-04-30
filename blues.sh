ad0aef474d7adae6829bfa649760a5330a9c9621
Justin Davis <Justin.Davis@slu.edu>
2019-04-30 14:13:21 -0500

    Add some newlines. format better

--- blues.sh
+++ blues.sh
@@ -4,9 +4,17 @@ wget -q https://www.cbssports.com/nhl/teams/STL/st-louis-blues/schedule/postseas
 GAMELEN=`cat index.html | egrep "$DATE" | wc -c`
 if [ $GAMELEN -gt 2 ]
 then
-	echo "There is a blues game today"
+	printf "There is a Blues game today\n\n"
 else
-	echo "There isn't a blues game today"
+	printf "There isn't a Blues game today\n\n"
 fi
-
+TOMORROW=`date --date="next day" "+%b %d, %Y"`
+TOMLEN=`cat index.html | egrep "$TOMDATE" | wc -c`
+echo "Tomorrow is" $TOMORROW
+if [ $TOMLEN -gt 2 ]
+then
+	printf "There is a Blues game tomorrow\n\n"
+else
+	printf "There isn't a Blues game tomorrow\n\n"
+fi
 rm index.html
