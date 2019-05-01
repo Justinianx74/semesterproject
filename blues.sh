7d38a9c0e091520538791cb5220d93f73678bb51
Justin Davis <Justin.Davis@slu.edu>
2019-05-01 14:14:26 -0500

    fixes date formatting

--- blues.sh
+++ blues.sh
@@ -1,17 +1,17 @@
-DATE=`date "+%b %d, %Y"`
+DATE=`date "+%b%_d, %Y"`
 echo "Today is" $DATE
 wget -q https://www.cbssports.com/nhl/teams/STL/st-louis-blues/schedule/postseason/
 GAMELEN=`cat index.html | egrep "$DATE" | wc -c`
-if [ $GAMELEN -gt 2 ]
+if [ $GAMELEN -gt 0 ]
 then
 	printf "There is a Blues game today\n\n"
 else
 	printf "There isn't a Blues game today\n\n"
 fi
-TOMORROW=`date --date="next day" "+%b %d, %Y"`
+TOMDATE=`date --date="next day" "+%b%_d, %Y"`
 TOMLEN=`cat index.html | egrep "$TOMDATE" | wc -c`
-echo "Tomorrow is" $TOMORROW
-if [ $TOMLEN -gt 2 ]
+echo "Tomorrow is" $TOMDATE
+if [ $TOMLEN -gt 0 ]
 then
 	printf "There is a Blues game tomorrow\n\n"
 else
